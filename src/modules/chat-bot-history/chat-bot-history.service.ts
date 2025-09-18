013e8f37a96038e690773016af13e28e05813f32
karthikiruba1210 <karthikiruba1210@gmail.com>
2025-09-18 19:07:54 +0530

    new chat bot

--- src/modules/chat-bot-history/chat-bot-history.service.ts
+++ src/modules/chat-bot-history/chat-bot-history.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { ChatBotHistory } from 'src/modules/chat-bot-history/entities/chat-bot-history.entity';
+import { NewChatBotHistory } from 'src/modules/chat-bot-history/entities/chat-bot-history.entity';
 
 @Injectable()
 export class ChatBotHistoryService {
   constructor(
-    @InjectRepository(ChatBotHistory)
-    private readonly historyRepository: Repository<ChatBotHistory>,
+    @InjectRepository(NewChatBotHistory)
+    private readonly historyRepository: Repository<NewChatBotHistory>,
   ) {}
 
   findAll() {
     return this.historyRepository.find({
-      order: { created_at: 'DESC' },
+      order: { id: 'DESC' },
     });
   }
 }
