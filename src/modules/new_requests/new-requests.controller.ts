d01b7e40b09fc0afff6c9776efef4c9ec1488a42
karthikiruba1210 <karthikiruba1210@gmail.com>
2025-09-18 11:16:21 +0530

    appoinment-management get api add in new requests api

--- src/modules/new_requests/new-requests.controller.ts
+++ src/modules/new_requests/new-requests.controller.ts
@@ -2,17 +2,25 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { NewRequestsService } from './new-requests.service';
 import { NewRequest } from './entities/new-request.entity';
 
-@Controller('new-requests')
+@Controller('new-requests-appointment-management')
 export class NewRequestsController {
   constructor(private readonly newRequestsService: NewRequestsService) {}
 
-  @Get()
-  async getAll(): Promise<NewRequest[]> {
-    return this.newRequestsService.findAll();
+
+
+    @Get()
+  async getDashboardData() {
+    return this.newRequestsService.getCombinedData();
   }
 
-  @Get(':id')
-  async getOne(@Param('id') id: number): Promise<NewRequest> {
-    return this.newRequestsService.findOne(id);
-  }
+  
+  // @Get()
+  // async getAll(): Promise<NewRequest[]> {
+  //   return this.newRequestsService.findAll();
+  // }
+
+  // @Get(':id')
+  // async getOne(@Param('id') id: number): Promise<NewRequest> {
+  //   return this.newRequestsService.findOne(id);
+  // }
 }
